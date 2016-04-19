d9ac221072ee693130f9b585a2dfcf3da6492fd3
keyboard3 <gcy15757106785@gmail.com>
2016-04-19 22:23:47 +0800

    源代码
    第一次实现源码（未重构）

---
+++ app/src/main/java/keyboard3/com/selfviewdemo/MainActivity.java
@@ -0,0 +1,47 @@
+package keyboard3.com.selfviewdemo;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import keyboard3.com.selfviewdemo.View.CircleView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CircleView circleView;
+    private TextView tv_num;
+    View view_line;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        circleView = (CircleView) findViewById(R.id.circle);
+        circleView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                circleView.startCustomAnimation();
+            }
+        });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                circleView.startCustomAnimation();
+            }
+        },1000);
+        tv_num= (TextView) findViewById(R.id.tv_num);
+        final int process=13140;
+        circleView.setProcessChangeLinstener(new CircleView.processChangeLinstener() {
+            @Override
+            public void onProcessChange(float interpolatedTime) {
+                tv_num.setText((int)(process*interpolatedTime)+"");
+            }
+        });
+        view_line = findViewById(R.id.view_line);
+        view_line.setLayerType(View.LAYER_TYPE_SOFTWARE,null);
+
+    }
+}
---
+++ app/src/main/java/keyboard3/com/selfviewdemo/View/ColumnView.java
@@ -0,0 +1,62 @@
+package keyboard3.com.selfviewdemo.View;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import keyboard3.com.selfviewdemo.R;
+
+/**
+ * Desc:
+ * Author: ganchunyu
+ * Date: 2016-04-19 13:29
+ */
+public class ColumnView extends View {
+    private int theme= Color.parseColor("#2EC3FD");
+    int mRadius;
+    int mHeight;
+    int foreColor;
+    Paint mPaint;
+    public ColumnView(Context context) {
+        super(context);
+        init();
+    }
+
+    public ColumnView(Context context, AttributeSet attrs) {
+        this(context,attrs,0);
+    }
+
+    public ColumnView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        final TypedArray typedArray=context.obtainStyledAttributes(attrs, R.styleable.ColumnView,defStyleAttr,0);
+        foreColor=typedArray.getColor(R.styleable.ColumnView_column_foreColor,theme);
+        init();
+    }
+    private void init(){
+        mPaint=new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setDither(true);
+        mPaint.setColor(foreColor);
+    }
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        mRadius=getMeasuredWidth()/2;
+        mHeight=getMeasuredHeight();
+        mPaint.setStrokeWidth(mRadius*2);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawCircle(mRadius,mRadius,mRadius,mPaint);
+        canvas.drawCircle(mRadius,mHeight-mRadius,mRadius,mPaint);
+        canvas.drawLine(mRadius,mRadius,mRadius,mHeight-mRadius,mPaint);
+    }
+}
